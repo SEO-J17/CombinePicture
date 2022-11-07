7d75b3d9b263e5a889b88280eb04e6f440389506
SEO-J17 <qw4643987@naver.com>
2022-11-08 03:28:08 +0900

    #1 feat: 이미지 불러오기 기능 추가

--- app/src/main/java/project/seo/combinepicture/MainActivity.kt
+++ app/src/main/java/project/seo/combinepicture/MainActivity.kt
@@ -2,10 +2,34 @@ package project.seo.combinepicture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import project.seo.combinepicture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var pictureView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            firstPicture.setOnClickListener {
+                pictureView = firstPicture
+                loadImage.launch("image/*")
+            }
+            secondPicture.setOnClickListener {
+                pictureView = secondPicture
+                loadImage.launch("image/*")
+            }
+        }
+
     }
+
+    private val loadImage =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            Glide.with(this@MainActivity).load(uri).into(pictureView)
+        }
 }
